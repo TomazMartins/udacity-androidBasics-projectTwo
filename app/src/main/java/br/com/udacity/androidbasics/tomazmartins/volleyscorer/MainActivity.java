63b3527a0d5c5f899bf3e9549dd581289c210442
TomazMartins <tomaz.r.martins@gmail.com>
2017-05-01 15:56:58 -0300

    Implementing events for reset and add aces
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- app/src/main/java/br/com/udacity/androidbasics/tomazmartins/volleyscorer/MainActivity.java
+++ app/src/main/java/br/com/udacity/androidbasics/tomazmartins/volleyscorer/MainActivity.java
@@ -4,10 +4,9 @@ import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import static android.R.attr.onClick;
 
 public class MainActivity extends AppCompatActivity {
     private TextView ref_score_set1_teamA;
@@ -38,10 +37,21 @@ public class MainActivity extends AppCompatActivity {
     private AppCompatButton btn_ace_teamA;
     private AppCompatButton btn_ace_teamB;
 
+    private int aces_teamA;
+    private int aces_teamB;
+    private int points_teamA;
+    private int points_teamB;
+
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        points_teamA = 0;
+        points_teamB = 0;
+        aces_teamA = 0;
+        aces_teamB = 0;
+
         setComponents();
         setButtons();
     }
@@ -63,15 +73,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onClickAddAceTeamA() {
+        btn_ace_teamA.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                addAceTeamA();
+            }
+        } );
+    }
 
+    private void addAceTeamA() {
+        ++aces_teamA;
+        ref_aces_teamA.setText( String.valueOf( aces_teamA ) );
     }
 
     private void onClickAddAceTeamB() {
+        btn_ace_teamB.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                addAceTeamB();
+            }
+        } );
+    }
 
+    private void addAceTeamB() {
+        ++aces_teamB;
+        ref_aces_teamB.setText( String.valueOf( aces_teamB ) );
     }
 
     private void onClickResetGame() {
+        btn_reset_match.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                resetGame();
+            }
+        } );
+    }
 
+    private void resetGame() {
+        resetSetPoints();
+        resetMatchPoints();
+    }
+
+    private void resetMatchPoints() {
+        ref_score_set1_teamA.setText( String.valueOf( 0 ) );
+        ref_score_set2_teamA.setText( String.valueOf( 0 ) );
+        ref_score_set3_teamA.setText( String.valueOf( 0 ) );
+        ref_score_set4_teamA.setText( String.valueOf( 0 ) );
+        ref_score_set5_teamA.setText( String.valueOf( 0 ) );
+        ref_aces_teamA.setText( String.valueOf( 0 ) );
+
+        ref_score_set1_teamB.setText( String.valueOf( 0 ) );
+        ref_score_set2_teamB.setText( String.valueOf( 0 ) );
+        ref_score_set3_teamB.setText( String.valueOf( 0 ) );
+        ref_score_set4_teamB.setText( String.valueOf( 0 ) );
+        ref_score_set5_teamB.setText( String.valueOf( 0 ) );
+        ref_aces_teamB.setText( String.valueOf( 0 ) );
+    }
+
+    private void resetSetPoints() {
+        score_teamA.setText( String.valueOf( 0 ) );
+        score_teamB.setText( String.valueOf( 0 ) );
     }
 
 
@@ -106,3 +167,4 @@ public class MainActivity extends AppCompatActivity {
         btn_reset_match = (AppCompatButton) findViewById( R.id.btn_reset_match );
     }
 }
+
